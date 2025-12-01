df48bdddd07a1b5cdad73158fad607648f95cbf1
KilviaSantos <kilviamssantos@gmail.com>
2025-11-30 23:22:39 -0300

    resolution ex10 while

---
+++ Ex10.py
@@ -0,0 +1,6 @@
+# Faça um algoritmo que exiba 20 vezes a mensagem
+# 'Eu gosto de estudar algoritmos', use while
+mensagem=1
+while (mensagem<=20):
+    print('Eu gosto de estudar algoritmos')
+    mensagem=mensagem+1
