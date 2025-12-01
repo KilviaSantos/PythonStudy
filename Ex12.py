c7fcae3ab4553610b1dbf0f41c2ba1d348588cbd
KilviaSantos <kilviamssantos@gmail.com>
2025-12-01 16:41:06 -0300

    resolution ex12 td

--- Ex12.py
+++ Ex12.py
@@ -1,10 +1,17 @@
 # Leia o nome e a idade de 10 pessoas
 # Exiba o nome e a idade da pessoa mais nova
 
-pessoa = 10
-while (pessoa>=1):
-    nome = input('Escreva seu nome: ')
-    idade = int(input('Escreva sua idade:'))
-    pessoa = pessoa-1
-    
+nome_menor = ''
+idade_menor = 0
+
+for _ in range(10):
+    nome = input('Digite o nome da pessoa: ')
+    idade = int(input('Digite a idade da pessoa: '))
+    if (idade_menor==0) or (idade<idade_menor):
+        nome_menor =nome 
+        idade_menor = idade
+        
+print('O(a) ', nome_menor, 'tem a menor idade que é: ', idade_menor, 'anos.' )
+
+
 
