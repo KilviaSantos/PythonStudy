60205d9f92c183024f94a3d4e82b50fcd61e4a93
KilviaSantos <kilviamssantos@gmail.com>
2025-12-01 17:03:29 -0300

    change exercise 7 td

--- Ex7.py
+++ Ex7.py
@@ -1,10 +1,9 @@
 # Armazene dois valores de notas de 20 alunos
 # Calcule a média entre as notas
 # Imprima a média de cada aluno
-for _ in range(3):
+for _ in range(20):
     nome = input('Nome do aluno: ')
     nota1 = float(input('Digite a primeira nota: '))
     nota2 = float(input('Digite a segunda nota: '))
     media_aluno= (nota1+nota2)/2
-    if media_aluno<=10:
-        print('A média do(a) aluno(a) foi: ', media_aluno)
+    print('A média do(a) aluno(a) foi: ', media_aluno)
