b74a29597d13c85e53d6fe3dcbc91d9272dc1c16
KilviaSantos <kilviamssantos@gmail.com>
2025-12-01 17:22:58 -0300

    resolution exercise 8 td

--- Ex8.py
+++ Ex8.py
@@ -1,2 +1,8 @@
 # Recebe um número do usuário
+num_choice = int(input('Escolha um número: '))
+pares = 0
 # Imprima os números pares do 1 até o número escolhido.
+while num_choice>= pares:
+    print(pares)
+    pares = pares+2
+   
