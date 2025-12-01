8c265b4b5bc6a1404f068f7a161aa83517f5e27f
KilviaSantos <kilviamssantos@gmail.com>
2025-11-30 23:10:36 -0300

    resolution ex6

---
+++ Ex6.py
@@ -0,0 +1,12 @@
+#Desenvolva um sistema que recebe o nome de cinco alunos, 
+nome=1
+posicao=0
+while (nome<=5):
+    aluno= input('Digite o nome do aluno: ')
+    posicao = posicao+1
+    print(posicao,'º', aluno)
+    nome=nome+1
+
+#depois imprime os nomes e a posição em que o aluno está armazenado
+
+ 
