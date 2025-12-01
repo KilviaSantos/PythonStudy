dc5cea700ecdde0697f79671b6472c456eabfe50
KilviaSantos <kilviamssantos@gmail.com>
2025-11-30 23:33:49 -0300

    resolution exercise td 11

---
+++ Ex11.py
@@ -0,0 +1,9 @@
+# Algoritmo que leia 10 números do usuário
+# Depois calcule a soma dos número recebidos, use while
+usuario = 3
+soma=0
+while (usuario>=1):
+    num = float(input('Digite um número:'))
+    soma=soma+num
+    usuario=usuario-1
+print('A soma dos números digitados é: ',soma)
